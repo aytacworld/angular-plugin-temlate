0710a2e0363eabfdd31175fffd1b1e0336832a3e
Adem Aytac <adem@aytacworld.com>
2017-10-01 21:27:33 +0200

    package version updated

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,16 @@
 const pkg = require('./package.json');
 
 export default {
-  dest: 'dist/bundles/' + pkg.name + '.umd.js',
-  entry: 'dist/index.js',
-  format: 'umd',
+  output: {
+    file: 'dist/bundles/' + pkg.name + '.umd.js',
+    format: 'umd'
+  },
+  input: 'dist/index.js',
   globals: {
     '@angular/core': 'ng.core',
     'rxjs': 'rxjs'
   },
-  moduleName: 'ng.simpleForms',
+  name: 'ng.simpleForms',
   sourceMap: false,
   external: [ '@angular/core', 'rxjs' ]
 }
